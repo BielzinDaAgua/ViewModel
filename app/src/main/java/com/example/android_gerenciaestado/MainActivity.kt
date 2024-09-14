ff4a77f06318c2e8f1ac3fbb31220c8b8a010090
20221370013 <gabriel_olivera_p@hotmail.com>
2024-09-13 18:50:23 -0600

    v1.1

--- app/src/main/java/com/example/android_gerenciaestado/MainActivity.kt
+++ app/src/main/java/com/example/android_gerenciaestado/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.android_gerenciaestado
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,37 +13,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android_gerenciaestado.ui.theme.Android_GerenciaEstadoTheme
 import com.example.android_gerenciaestado.ui.viewmodels.ContadorViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Android_GerenciaEstadoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                        ContadorStateful(
-                            modifier = Modifier.padding(innerPadding)
+                    val viewModel by viewModels<ContadorViewModel>()
+                    val contador by viewModel.contador.collectAsState()
+
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        ContadorStateless(
+                            contador = contador,
+                            onIncrementContador = { viewModel.incrementar() },
+                            onDecrementContador = { viewModel.decrementar() }
                         )
-
-
-
-
-
-
-//                        var contador by remember { mutableIntStateOf(0) }
-                        val viewModel by viewModels<ContadorViewModel>()
-                        val contador by viewModel.contador.collectAsState()
-                        ContadorStateless(contador, onIncrementContador = {viewModel.incrementar()}, modifier = Modifier.padding(innerPadding))
-                        ContadorStateless(contador, onIncrementContador = {viewModel.incrementar()}, modifier = Modifier.padding(innerPadding))
                     }
                 }
             }
@@ -53,43 +43,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ContadorStateless(contador: Int, onIncrementContador: () -> Unit, modifier: Modifier = Modifier) {
-
+fun ContadorStateless(
+    contador: Int,
+    onIncrementContador: () -> Unit,
+    onDecrementContador: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "Contador: $contador"
-        )
+        Text(text = "Contador: $contador")
+
         Button(onClick = onIncrementContador) {
             Text("Incremente aqui")
-
         }
 
-    }
-
-}
-
-
-@Composable
-fun ContadorStateful(modifier: Modifier = Modifier) {
-    var contador by remember { mutableIntStateOf(0) }
-
-    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "Contador: $contador"
-        )
-        Button(onClick = {contador++}) {
-            Text("Incremente aqui")
-
+        Button(onClick = onDecrementContador, modifier = Modifier.padding(top = 8.dp)) {
+            Text("Decremente aqui")
         }
-
     }
-
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Android_GerenciaEstadoTheme {
+        ContadorStateless(contador = 0, onIncrementContador = {}, onDecrementContador = {})
     }
 }
